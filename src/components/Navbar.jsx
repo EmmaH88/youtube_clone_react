bd9d21243527204ddc0296f962973f193585eb30
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-10 11:53:36 +0000

    corrected spelling in utils folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import { logo } from "./utils/constant";
+const Navbar = () => (
+  <stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      backgroun: "#000",
+      top: 0,
+      justifyContent: "space-bteween",
+    }}
+  >
+    <link
+      to="/"
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <img src={logo} alt="logo" height={45} />
+    </link>
+  </stack>
+);
 
 export default Navbar;
