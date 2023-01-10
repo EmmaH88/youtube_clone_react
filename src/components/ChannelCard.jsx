1c0b83223cd24a10b1d00e391bd13903bd187a83
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-10 15:28:33 +0000

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,8 +3,29 @@ import { CheckCircles } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+const ChannelCard = (channelDetail) => {
+  return (
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia image={channelDetail?.snippet?.thumbnails?.high} />
+        </CardContent>
+      </Link>
+    </Box>
+  );
 };
 
 export default ChannelCard;
