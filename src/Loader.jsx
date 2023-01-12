52a3c7ced9ba3a2288df7000c1d25efd6e47dc3d
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-12 12:28:26 +0000

    Update Loader.jsx

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { Box, CircularProgress, Stack } from "@mui/material";
 
-const Loader = () => {
-  return (
-    <Box minHeight="95vh">
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        height="80vh"
-      >
-        <CircularProgress />
-      </Stack>
-    </Box>
-  );
-};
+const Loader = () => (
+  <Box minHeight="95vh">
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      height="80vh"
+    >
+      <CircularProgress />
+    </Stack>
+  </Box>
+);
 
 export default Loader;
