abcc01c9ce18b9a84b4b921a345e4c10ccc0ae19
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-12 12:56:20 +0000

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,13 +1,16 @@
+import React from "react";
 import { Stack, Box } from "@mui/material";
-import { VideoCard, ChannelCard } from "./";
+
+import { VideoCard, Loader, ChannelCard } from "./";
 
 const Videos = ({ videos, direction }) => {
-  if (!videos?.length) return "Loading...";
+  if (!videos?.length) return <Loader />;
   return (
     <Stack
       direction={direction || "row"}
       flexWrap="wrap"
       justifyContent="start"
+      alignItems="start"
       gap={2}
     >
       {videos.map((item, idx) => (
