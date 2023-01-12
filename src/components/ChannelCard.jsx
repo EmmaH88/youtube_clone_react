38faeaa26180be890b6be1937370fa0f8f97c4f2
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-12 12:11:54 +0000

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, CardContent, CardMedia, Typography } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material.CheckCricle";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
